4f3ef683e513b9da78a5ae43f6a5625c9364852d
Anjali041 <anjali041kumari@gmail.com>
2021-01-15 16:28:31 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var todoController= require('./controllers/todoController');
+
+var app= express();
+
+app.set('view engine', 'ejs');
+
+//static files
+app.use(express.static('./public'));
+
+todoController(app);
+
+app.listen(3001);
+console.log('You are listening to port 3001');
